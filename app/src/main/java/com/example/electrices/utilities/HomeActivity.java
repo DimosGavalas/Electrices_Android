bf78c868d4aff3601b3c84dbcef4e04da7b47c6d
elenagudzh <44157910+elenagudzh@users.noreply.github.com>
2021-04-28 15:38:48 +0200

    statistics wireframe

--- app/src/main/java/com/example/electrices/utilities/HomeActivity.java
+++ app/src/main/java/com/example/electrices/utilities/HomeActivity.java
@@ -2,15 +2,14 @@ package com.example.electrices.utilities;
 
 import android.os.Bundle;
 
-import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.electrices.R;
 
-public class StatisticsPricesActivity extends AppCompatActivity {
+public class HomeActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.prices_screen_statistics);
+        setContentView(R.layout.home_screen);
     }
 }

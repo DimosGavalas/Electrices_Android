28052a0d5396788de2e9109bd187334fff939c1f
elenagudzh <44157910+elenagudzh@users.noreply.github.com>
2021-04-28 15:32:12 +0200

    statistics wireframe

---
+++ app/src/main/java/com/example/electrices/utilities/StatisticsPricesActivity.java
@@ -0,0 +1,16 @@
+package com.example.electrices.utilities;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.electrices.R;
+
+public class StatisticsPricesActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.prices_screen_statistics);
+    }
+}
